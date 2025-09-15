d33c49d5585bf8cbf7d1eb0232d3440f4a454484
khushigupta86 <khushiigupta086@gmail.com>
2025-09-15 21:04:35 +0530

    signup controller added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,58 @@
+import User from "../models/user.model.js";
+import bycrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+
+
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+};
+
+try {
+   if(!fullName || !email || !password){
+    return res.status(400).json({message:"All fields are required"}); 
+   }
+
+   if(password.length < 6){
+    return res.status(400).json({message:"Password must be at least 6 characters"});
+   }
+
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   if(!emailRegex.test(email)){
+    return res.status(400).json({message:"Please enter a valid email"});
+   }
+
+   const user= await User.findOne({email});
+    if(user){
+        return res.status(400).json({message:"User already exists"});
+    }
+    
+    const salt = await bycrypt.genSalt(10);
+    const hashedPassword = await bycrypt.hash(password,salt);
+
+    const newUser = new User({
+        fullName,
+        email,
+        password:hashedPassword,
+    });
+
+    if(newUser){
+        generateToken(newUser._id);
+        await newUser.save();
+
+        res.status(201).json({
+            _id:newUser._id,
+            fullName:newUser.fullName,
+            email:newUser.email,
+            profilePic:newUser.profilePic,
+        });
+    }
+    else{
+        res.status(400).json({message:"Invalid user data"});
+    }
+    
+
+
+} catch (error) {
+    console.log("Error in signup:",error);
+    res.status(500).json({message:"Interval Server error"});
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,8 @@ import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+
 
 
 
@@ -17,12 +19,14 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json()); //req.body
+
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
 
-//make raedy for deployment
+//make ready for deployment
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../chatapp/dist")));
 
@@ -32,7 +36,7 @@ if(process.env.NODE_ENV==="production"){
 }
 
 
-
-app.listen(PORT, () => 
-  console.log("Server is running on port:",+PORT)
-);                                                  
+app.listen(PORT, () => {
+    console.log("Server running on port:" + PORT);
+    connectDB();
+});                                                  
